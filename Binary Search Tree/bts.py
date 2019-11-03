6d89db1106268f772b9b46a5b26450f8cdadee37
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-11-03 10:23:06 +0600

    class BinarySearchTree

--- Binary Search Tree/bts.py
+++ Binary Search Tree/bts.py
@@ -1,7 +1,30 @@
 class Node(object):
 
-    def __init__(object):
+    def __init__(self):
         self.data = data;
         self.leftChild = None;
         self.rightChild = None;
-        
+
+class BinarySearchTree(object):
+
+    def __init__(self):
+        self.root = None;
+
+    def insert(self, data):
+        if not self.root:
+            self.root = Node(data);
+        else:
+            self.insertNode(data, self.root);
+
+    def insertNode(self, data, node):
+
+        if data < node.data:
+            if node.leftChild:
+                self.insertNode(data, node.leftChild);
+            else:
+                node.leftChild = Node(data);
+        else:
+            if node.rightChild:
+                self.insertNode(data, node.rightChild);
+            else:
+                node.rightChild = Node(data);
