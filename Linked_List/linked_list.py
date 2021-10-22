5904ee1efbe7845bd610b116220c1ac33db4697a
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-10-22 09:19:51 +0200

    Node adding at end plus traverse

--- Linked_List/linked_list.py
+++ Linked_List/linked_list.py
@@ -11,7 +11,7 @@ class LinkedList(object):
         self.head = None
         self.size = 0
 
-    def insert_node(self, data):
+    def insert_start(self, data):
         self.size = self.size + 1
         new_node = Node(data)
 
@@ -22,9 +22,30 @@ class LinkedList(object):
             new_node.next_node = self.head
             self.head = new_node
 
+    def insert_end(self, data):
+        self.size = self.size+1
+        new_node = Node(data)
+        actual_node = self.head
+
+        while actual_node.next_node is not None:
+            actual_node = actual_node.next_node
+
+        actual_node.next_node = new_node
+
+    def traverse(self):
+        actual_node = self.head
+
+        while actual_node is not None:
+            print(actual_node.data)
+            actual_node = actual_node.next_node
+
+
 
 linked_list = LinkedList()
-linked_list.insert_node(5)
-linked_list.insert_node(45)
-linked_list.insert_node(85)
-print(linked_list.size)
+linked_list.insert_start(5)
+linked_list.insert_start(45)
+linked_list.insert_start(85)
+linked_list.insert_end(100)
+print("Size: ", linked_list.size)
+print("Traversing")
+linked_list.traverse()
