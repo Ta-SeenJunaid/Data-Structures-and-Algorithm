3344a27f096b6edc1905281269e9a9d3b341c98a
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-11-06 11:35:01 +0100

    Height and Balance calculation

--- AVL Tree/avl_tree_revision.py
+++ AVL Tree/avl_tree_revision.py
@@ -1,8 +1,24 @@
 
-class Node(object):
+class Node:
 
     def __init__(self, data):
         self.data = data
         self.height = 0
         self.left_child = None
         self.right_child = None
+
+    @staticmethod
+    def calc_height(self, node):
+        if not node:
+            return -1
+
+        return node.height
+
+    def calc_balance(self, node):
+
+        if not node:
+            return 0
+
+        return self.calc_height(node.left_child) - self.calc_height(node.right_child)
+
+
