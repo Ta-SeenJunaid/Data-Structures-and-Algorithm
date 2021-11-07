f42c45049dbb8da463523d4364f390014cd8f7d5
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-11-07 18:35:57 +0100

    Insertion and making balance

--- AVL Tree/avl_tree_revision.py
+++ AVL Tree/avl_tree_revision.py
@@ -13,6 +13,44 @@ class AVL:
     def __init__(self):
         self.root = None
 
+    def insert(self, data):
+        self.root = self.insert_node(data, self.root)
+
+    def insert_node(self, data, node):
+
+        if not node:
+            return Node(data)
+
+        if data < node.data:
+            node.left_child = self.insert_node(data, node.left_child)
+        else:
+            node.right_child = self.insert_node(data, node.right_child)
+
+        node.height = max(self.calc_height(node.left_child), self.calc_height(node.right_child)) + 1
+        return self.settle_violation(data, node)
+
+    def settle_violation(self, data, node):
+
+        balance = self.calc_balance(node)
+
+        if balance > 1 and data < node.left_child.data:
+            print("Left left heavy situation")
+            return self.rotate_right(node)
+        if balance < -1 and data > node.right_child.data:
+            print("Right right heavy situation")
+            return self.rotate_left(node)
+        if balance > 1 and data > node.left_child.data:
+            print("Left right heavy situation")
+            node.left_child = self.rotate_left(node)
+            return self.rotate_right(node)
+        if balance < -1 and data < node.right_child.data:
+            print("Right left heavy situation")
+            node.right_child = self.rotate_right(node)
+            return self.rotate_left(node)
+
+        return node
+
+
     @staticmethod
     def calc_height(self, node):
         if not node:
