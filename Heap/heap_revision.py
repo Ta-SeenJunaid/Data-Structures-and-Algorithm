de823458dce2c918f8b10e804ea19a2d6aede956
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-12-04 06:23:19 +0100

    heap_sort

--- Heap/heap_revision.py
+++ Heap/heap_revision.py
@@ -1,7 +1,7 @@
 
 class Heap:
 
-    HEAP_SIZE = 10
+    HEAP_SIZE = 100
 
     def __init__(self):
         self.heap = [0] * self.HEAP_SIZE
@@ -23,7 +23,7 @@ class Heap:
             return False
 
     def heap_sort(self):
-        for i in range(0, self.current_position):
+        for i in range(0, self.current_position + 1):
             temp = self.heap[0]
             print(temp)
             self.heap[0] = self.heap[self.current_position - i]
@@ -39,5 +39,42 @@ class Heap:
             self.heap[parent_index] = temp
             parent_index = int((parent_index-1)/2)
 
-    def fix_down(self, start, end):
-        pass
+    def fix_down(self, index, up_to):
+        while index <= up_to:
+            left_child = 2 * index + 1
+            right_child = 2 * index + 2
+            if left_child < up_to:
+                child_to_swap = None
+                if right_child > up_to:
+                    child_to_swap = left_child
+                else:
+                    if self.heap[left_child] > self.heap[right_child]:
+                        child_to_swap = left_child
+                    else:
+                        child_to_swap = right_child
+                if self.heap[index] < self.heap[child_to_swap]:
+                    temp = self.heap[index]
+                    self.heap[index] = self.heap[child_to_swap]
+                    self.heap[child_to_swap] = temp
+                else:
+                    break
+                index = child_to_swap
+            else:
+                break
+
+
+heap = Heap()
+heap.insert(15)
+heap.insert(35)
+heap.insert(10)
+heap.insert(11)
+heap.insert(5)
+heap.insert(-2)
+heap.insert(-25)
+heap.insert(100)
+heap.insert(500)
+heap.insert(10000)
+heap.insert(-200)
+heap.insert(23)
+
+heap.heap_sort()
