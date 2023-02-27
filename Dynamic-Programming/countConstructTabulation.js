8148a92434281cfeffa33a81bc54afe4fa8473ad
Ta-SeenJunaid <taseen.junaid@gmail.com>
2023-02-27 13:26:00 +0100

    countConstructTabulation.js

---
+++ Dynamic-Programming/countConstructTabulation.js
@@ -0,0 +1,26 @@
+const countConstruct = (target, wordBank) => {
+    const table = Array(target.length + 1).fill(0);
+    table[0] = 1;
+    for (let i = 0; i <= target.length; i++){
+        for (let word of wordBank) {
+            if (target.slice(i, i + word.length) === word){
+                table[i + word.length] += table[i];
+            }
+        }
+    }
+
+    return table[target.length]
+};
+
+
+console.log(countConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))
+console.log(countConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']));
+console.log(countConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]));
+console.log(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+    "e", 
+    "ee", 
+    "eee", 
+    "eeee", 
+    "eeeee", 
+    "eeeeee"
+]));
